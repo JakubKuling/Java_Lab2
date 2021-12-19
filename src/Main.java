0c3780caf3fee7219db982496bcdcf99c1fe869e
kubak <kuba8983@wp.pl>
2021-12-19 13:42:33 +0100

    zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 public class Main {
+
 
     public static void main(String[] args) {
 
@@ -8,11 +10,37 @@ public class Main {
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
+        int i = 100;
+        int dzielnik = 80;
+        for (; i > 0; ) {
+            System.out.print(i);
+            if (i % dzielnik == 0) {
+
+                System.out.print("  podzielna przez " + dzielnik);
+            }
+            System.out.println();
+            i--;
+        }
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
 
+        int[] w = new int[5];
+        int z = 0;
+
+        Scanner scan = new Scanner(System.in);
+        for (int element : w) {
+            System.out.println("\nPodaj " + (z + 1) + " element tablicy: ");
+
+            w[z] = scan.nextInt();
+            z++;
+        }
+        for (int value : w) {
+            System.out.println(value + 11);
+        }
+
 
     }
 }
+
